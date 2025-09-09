ad6b6a1396cfb6b5997b297fef5d4d3c28ad495b
Tshepiso <u23588579@tuks.co.za>
2025-09-10 01:13:31 +0200

    Basic structure setup

--- Users.h
+++ Users.h
@@ -1,10 +1,21 @@
 #ifndef USERS_H
 #define USERS_H
 
+#include <string>
+#include <vector>
 
+#include <iostream>
 
 class Users {
+    protected : 
+    std::string name;
+    //vector of chatrooms callled chatrooms
+    std::vector<ChatRoom*> chatrooms;
 
+    public :
+    Users(ChatRoom* chatroom, std::string name);
+    void sendMessage(std::string message, ChatRoom* chatroom);
+    void receiveMessage(std::string message, Users* fromUser, ChatRoom* chatroom);
 };
 
 
