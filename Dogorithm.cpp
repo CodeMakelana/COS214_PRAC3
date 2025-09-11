bf61fa9239033085e96e7d73184e1529b72e8476
Tshepiso <u23588579@tuks.co.za>
2025-09-11 15:13:58 +0200

    Implemeted the 2nd chatroom

--- Dogorithm.cpp
+++ Dogorithm.cpp
@@ -1,1 +1,33 @@
 #include "Dogorithm.h"
+
+Dogorithm::Dogorithm() {
+    //initialize the users and chatHistory lists
+    users = std::list<Users*>();
+    chatHistory = std::list<std::string>();
+}
+
+Dogorithm::~Dogorithm() {
+    //clear the users and chatHistory lists
+    users.clear();
+    chatHistory.clear();
+}
+
+void Dogorithm::registerUser(User user) {
+    this.users.add(user);
+}
+
+void Dogorithm::sendMessage(std::string mess, User fromUser) {
+    for (User u : this.users) {
+        if (u != fromUser) {
+            u.receiveMessage(mess, fromUser, this);
+        }
+    }
+}
+
+void Dogorithm::saveMessage(std::string mess, User fromUser) {
+    this.chatHistory.add(fromUser.getName() + ": " + mess);
+}
+
+void Dogorithm::removeUser(User user) {
+    this.users.remove(user);
+}
--- Dogorithm.h
+++ Dogorithm.h
@@ -1,10 +1,16 @@
 #ifndef DOGORITHM_H
 #define DOGORITHM_H
 
+#include "ChatRoom.h"
 
-
-class Dogorithm {
-
+class Dogorithm : public ChatRoom {
+    public:
+    Dogorithm();
+    ~Dogorithm();
+    void registerUser (Users user);
+    void sendMessage (string message, Users fromUser);
+    void saveMessage (string message, Users fromUser);
+    void removeUser (Users user);
 };
 
 
