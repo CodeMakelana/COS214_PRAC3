ba96578c6fccf968a8a2a80d60f4945b78206770
Tshepiso <u23588579@tuks.co.za>
2025-09-11 15:10:48 +0200

    Implementation of Cindy (a user)

--- Cindy.cpp
+++ Cindy.cpp
@@ -1,1 +1,14 @@
 #include "Cindy.h"
+
+Cindy::Cindy(ChatRoom* chatroom, std::string name) 
+        : Users(chatroom, name) {}
+
+Cindy::~Cindy() {
+    // Destructor logic if needed
+}
+
+void Cindy::joinChatRoom(ChatRoom* chatroom) {
+    // Add the chatroom to Cindy's list and register her in that chatroom
+    this->chatrooms.push_back(chatroom);
+    chatroom->registerUser(*this);
+}
--- Cindy.h
+++ Cindy.h
@@ -1,9 +1,18 @@
 #ifndef CINDY_H
 #define CINDY_H
 
+#include "Users.h"
+#include "CtrlCat.h"
+#include "Dogorithm.h"
 
 
-class Cindy {
+class Cindy : public Users{
+    public :
+    Cindy(ChatRoom* chatroom, std::string name);
+    ~Cindy();
+
+    //for more than 1 chatrooms
+    void joinChatRoom(ChatRoom* chatroom);
 
 };
 
