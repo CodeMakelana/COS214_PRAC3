3d3e0094e6dfc7e7b866aad4144c87485f6690c0
Tshepiso <u23588579@tuks.co.za>
2025-09-11 15:23:53 +0200

    Implemented the 2nd user

--- Ezio.cpp
+++ Ezio.cpp
@@ -1,1 +1,8 @@
 #include "Ezio.h"
+
+Ezio::Ezio(ChatRoom* chatroom, std::string name) 
+        : Users(chatroom, name) {}
+
+Ezio::~Ezio() {
+    // Destructor logic if needed
+}
--- Ezio.h
+++ Ezio.h
@@ -1,10 +1,13 @@
 #ifndef EZIO_H
 #define EZIO_H
 
+#include "Users.h"
+#include "Dogorithm.h"
 
-
-class Ezio {
-
+class Ezio : public Users {
+    public:
+    Ezio(ChatRoom* chatroom, std::string name);
+    ~Ezio();
 };
 
 
