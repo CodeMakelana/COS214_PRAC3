5fc681844f0079b767654cd33f671a4463842dd7
Tshepiso <u23588579@tuks.co.za>
2025-09-11 17:34:55 +0200

    Improved testing

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -18,18 +18,21 @@ int main() {
 
     //Create users
     Cindy* cindy = new Cindy(Catroom, "Cindy");
-    cindy->joinChatRoom(Dogroom); //since Cindy is aprt of 2 chatrooms
+    cindy->joinChatRoom(DogRoom); //since Cindy is aprt of 2 chatrooms
 
     //users in single rooms
     Ezio* ezio = new Ezio(DogRoom, "Ezio");
-    Cole* cole = new Cindy(Catroom, "Cole");
+    Cole* cole = new Cole(Catroom, "Cole");
 
     //egister the users (join them in the chatrooms)
-    Catroom->registerUser(cindy);
-    Catroom->registerUser(cole);
-    DogRoom->registerUser(ezio);
+    // Catroom->registerUser(*cindy);
+    // Catroom->registerUser(*cole);
+    // DogRoom->registerUser(*ezio);
 
     ezio->sendMessage("Hello", DogRoom);
+    cole->sendMessage("My cat is the best", Catroom);
+    cindy->sendMessage("I love dogs", DogRoom);
+    cindy->sendMessage("I love cats", Catroom);
     return 0;
     
 }
