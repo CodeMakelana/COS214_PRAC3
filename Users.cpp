cb9a8a58041aadbb12c4a88aff2361b364429c6a
Tshepiso <u23588579@tuks.co.za>
2025-09-11 14:54:32 +0200

    Just the basic mediator implementations, w/o command.

--- Users.cpp
+++ Users.cpp
@@ -1,1 +1,24 @@
 #include "Users.h"
+
+Users::Users(ChatRoom* chatroom, std::string name) {
+    this->name = name;
+    this->chatrooms.push_back(chatroom);
+    for (ChatRoom* cr : chatrooms) {
+        cr->registerUser(*this);
+    }
+}
+
+Users::~Users() {
+    for (ChatRoom* cr : chatrooms) {
+        cr->removeUser(*this);
+    }
+}
+
+void Users::sendMessage(std::string message, ChatRoom* chatroom) {
+    chatroom->sendMessage(message, *this);
+}
+
+void Users::receiveMessage(std::string message, Users* fromUser, ChatRoom* chatroom) {
+    std::cout << this->name<< " in chatroo" << " received a message from " << fromUser->name << " in chatroom: " << message << std::endl;
+    chatroom->saveMessage(message, *fromUser);
+}
