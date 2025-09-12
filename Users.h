13c46b2c56913b73da6ed36174b64b86f4f171d3
CodeMakelana <u23588579@tuks.co.za>
2025-09-13 00:03:37 +0200

    Integrated the command pattern into the mediator part

--- Users.h
+++ Users.h
@@ -7,11 +7,15 @@
 #include <iostream>
 #include "ChatRoom.h"
 
+class Command; // Forward declaration of Command class
+
 class Users {
     protected : 
     std::string name;
     //vector of chatrooms callled chatrooms
     std::vector<ChatRoom*> chatrooms;
+    //vector of commands called commands
+    std::vector<Command*> commands;
 
     public :
     Users(ChatRoom* chatroom, std::string name);
@@ -19,6 +23,8 @@ class Users {
     void send(std::string message, ChatRoom* chatroom);
     void receive(std::string message, Users* fromUser, ChatRoom* chatroom);
     std::string getName() const { return name; }
+    void addCommand(Command* command);
+    void execuiteAll();
 };
 
 
