0e942683d4b49a4695aaba9f57980a61e13d317c
CodeMakelana <u23588579@tuks.co.za>
2025-09-29 12:43:13 +0200

    Fixed the override issue

--- Dogorithm.h
+++ Dogorithm.h
@@ -11,9 +11,9 @@ class Dogorithm : public ChatRoom {
     Dogorithm();
     ~Dogorithm();
     void registerUser (Users& user);
-    void sendMessage (std::string message, Users& fromUser);
-    void saveMessage (std::string message, Users& fromUser);
-    void removeUser (Users& user);
+    void sendMessage (std::string message, Users& fromUser) override;
+    void saveMessage (std::string message, Users& fromUser) override;
+    void removeUser (Users& user) override;
 
     Iterator<Users*>* createUsersIterator() override;
     Iterator<std::string*>* createChatHistoryIterator() override;
