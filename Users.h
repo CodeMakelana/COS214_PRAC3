67acb16662875727c3bcdaffb5130db5eaa04550
CodeMakelana <u23588579@tuks.co.za>
2025-09-29 11:55:34 +0200

    Updated the Users to work with the strategy pattern on how to format messages

--- Users.h
+++ Users.h
@@ -3,9 +3,11 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include <iostream>
 #include "ChatRoom.h"
-#include "Command.h"                                                                                                                        
+#include "Command.h"    
+#include "Strategy.h"
 
 class Users {
     protected : 
@@ -14,6 +16,7 @@ class Users {
     std::vector<ChatRoom*> chatrooms;
     //vector of commands called commands
     std::vector<Command*> commands;
+    MessageFormattingStrategy* messageStrategy; // Strategy for message formatting
 
     public :
     Users(ChatRoom* chatroom, std::string name);
@@ -23,8 +26,12 @@ class Users {
     std::string getName() const { return name; }
     void addCommand(Command* command);
     void execuiteAll();
+
+    // Strategy pattern methods
+    void setMessageStrategy(MessageFormattingStrategy* strategy);
+    MessageFormattingStrategy* getMessageStrategy();
+
+    std::string getName() { return name; }
 };
-
-
 
 #endif //USERS_H
